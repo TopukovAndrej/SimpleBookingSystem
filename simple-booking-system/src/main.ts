f262a130928c99f72067622961ef3b9a4020feed
Andrej Topukov <88235830+TopukovAndrej@users.noreply.github.com>
2025-02-27 18:47:15 +0100

    Init

---
+++ simple-booking-system/src/main.ts
@@ -0,0 +1,8 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { coreConfig } from './app/core/core.config';
+
+bootstrapApplication(AppComponent, {
+  providers: [...appConfig.providers, ...coreConfig.providers],
+}).catch((err) => console.error(err));
